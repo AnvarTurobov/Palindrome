873dc363298fc0323cd49bd5c087bc45b691a7af
AnvarTurobov <anvar.turobov@gmail.com>
2015-11-02 17:38:37 +0000

    added pseudocode

---
+++ palindrome-counter/counter.rb
@@ -0,0 +1,5 @@
+# create a method to go through and open each test file - (StringScanner)
+# create a method to i_to_s and check if palindrome? (str = str.reverse)
+# create a method to << detected palindromes into []
+# create method to sort and count palindromes
+# print the outcome for each file
